199ef360e8033eb0ba8fe445ba04becd525c74a4
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-03-21 18:13:12 +0300

    Update app-server.py

--- app/web/projects/mlb-lab/app-server.py
+++ app/web/projects/mlb-lab/app-server.py
@@ -1,5 +1,5 @@
 from multiprocessing import Process
-from admin.auth import app as admin_app  # Importing app from admin/auth.py
+from auth import app as admin_app  # Importing app from admin/auth.py
 from server_convertor import app as main_app  # Importing app from server_convertor.py
 import os
 
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         os.system("gunicorn -b 0.0.0.0:7000 server_convertor:app")
 
     def run_admin_app():
-        os.system("gunicorn -b 0.0.0.0:3000 admin.auth:app")
+        os.system("gunicorn -b 0.0.0.0:3000 auth:app")
 
     # Create and start processes for each Flask app
     main_process = Process(target=run_main_app)
