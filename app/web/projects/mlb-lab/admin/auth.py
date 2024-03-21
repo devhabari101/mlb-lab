30d273bcedd0a67b8a5b387a5656aafcfbfc88a0
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-03-21 11:45:12 +0300

    Update auth.py

--- app/web/projects/mlb-lab/admin/auth.py
+++ app/web/projects/mlb-lab/admin/auth.py
@@ -2,19 +2,16 @@ from flask import Flask, render_template, redirect, url_for
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
-load_dotenv()
 
 app = Flask(__name__)
 
-# Define PostgreSQL connection URL
-POSTGRES_URL = os.getenv("POSTGRES_URL") 
+# Define SQLite3 database file path
+DB_FILE_PATH = 'sqlite:///admin/mydb.db'
 
 # Create engine and session
-engine = create_engine(POSTGRES_URL)
+engine = create_engine(DB_FILE_PATH)
 Session = sessionmaker(bind=engine)
 session = Session()
 
