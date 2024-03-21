1eec4f9028e1db86ed15c78eb7d0f4d0fa98282f
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-03-21 15:22:29 +0300

    Update app-server.py

--- app/web/projects/mlb-lab/app-server.py
+++ app/web/projects/mlb-lab/app-server.py
@@ -1,23 +1,23 @@
-from threading import Thread
-from flask import Flask
+from multiprocessing import Process
 from admin.auth import app as admin_app  # Importing app from admin/auth.py
 from server_convertor import app as main_app  # Importing app from server_convertor.py
+import os
 
 if __name__ == '__main__':
-    # Define functions to run each Flask app in a separate thread
+    # Define functions to run each Flask app in a separate process
     def run_main_app():
-        main_app.run(host="0.0.0.0", debug=True, port=7000)
+        os.system("gunicorn -b 0.0.0.0:7000 server_convertor:app")
 
     def run_admin_app():
-        admin_app.run(host="0.0.0.0", debug=True, port=3000)
+        os.system("gunicorn -b 0.0.0.0:3000 admin.auth:app")
 
-    # Create and start threads for each Flask app
-    main_thread = Thread(target=run_main_app)
-    admin_thread = Thread(target=run_admin_app)
+    # Create and start processes for each Flask app
+    main_process = Process(target=run_main_app)
+    admin_process = Process(target=run_admin_app)
 
-    main_thread.start()
-    admin_thread.start()
+    main_process.start()
+    admin_process.start()
 
-    # Wait for both threads to finish
-    main_thread.join()
-    admin_thread.join()
+    # Wait for both processes to finish
+    main_process.join()
+    admin_process.join()
