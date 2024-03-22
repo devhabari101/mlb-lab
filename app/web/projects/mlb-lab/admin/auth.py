cf0903f675eaf345da60083259140b5f4f7b1308
devhabari101 <125182471+devhabari101@users.noreply.github.com>
2024-03-22 20:20:12 +0300

    Update auth.py

--- app/web/projects/mlb-lab/admin/auth.py
+++ app/web/projects/mlb-lab/admin/auth.py
@@ -13,7 +13,7 @@ import os
 
 app = Flask(__name__, template_folder='admin_templates')
 # Set secret key using the SECRET_KEY environment variable
-app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 csrf = CSRFProtect(app)  # Initialize CSRF protection
 login_manager = LoginManager(app)
 
